62736cea6f0d8d77f0df87a4e3e2f44789cf9dcd
Arno736 <52139210+Arno736@users.noreply.github.com>
2021-02-05 23:18:13 +0100

    v1.2 Changes & Bug Fixes

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ module.exports = function ServerIdentifier(mod) {
 		let str = ((!serverId[id]) ? ('Unk' + id) :  serverId[id]);
 		return ((!name) ? str : (name + ' | ' + str));
 	}
-
-	//Refresh au spawn
+	
+	//Refresh At Players Spawn
 	mod.hook('S_SPAWN_USER', 17, (event) => {
 		if(enabled) {
 			event.guildName = guildName(event.guildName, event.serverId);
@@ -27,23 +27,8 @@ module.exports = function ServerIdentifier(mod) {
 		}
 	});
 
-	//Données du groupe
-	let group = {};
-	mod.hook('S_PARTY_MEMBER_LIST', 8, (event) => {
-		if (!enabled) return;
-
-		group = {};
-
-		event.members.forEach(element => {
-			group[element.gameId] = event.serverId
-		});
-	});
-
-	//Refresh en groupe
+	//Block Guild Refresh For Other Player
 	mod.hook('S_GUILD_NAME', 2, (event) => {
-		if (enabled && event.gameId != mod.game.me.gameId && group.length > 0 && group[event.gameId]) {
-			event.guildName = guildName(event.guildName,  group[event.gameId]);
-			return true;
-		}
+		if (enabled && event.gameId != mod.game.me.gameId) return false;
 	});
 }
